5472460afa2cd46af0505d4ca664b74f3a57a63b
Hossam Ghareeb <hossam.ghareb@gmail.com>
2015-11-21 15:31:30 +0400

    Using NSOperationQueue.mainQueue()

--- ConcurrencyDemo/ConcurrencyDemo/ViewController.swift
+++ ConcurrencyDemo/ConcurrencyDemo/ViewController.swift
@@ -47,36 +47,31 @@ class ViewController: UIViewController {
         queue.addOperationWithBlock { () -> Void in
             
             let img1 = Downloader .downloadImageWithURL(imageURLs[0])
-            
-            dispatch_async(dispatch_get_main_queue(), {
-                
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imagView1.image = img1
             })
         }
         
         queue.addOperationWithBlock { () -> Void in
-            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
             
-            dispatch_async(dispatch_get_main_queue(), {
-                
+            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView2.image = img2
             })
         }
         
         queue.addOperationWithBlock { () -> Void in
-            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
             
-            dispatch_async(dispatch_get_main_queue(), {
-                
+            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView3.image = img3
             })
         }
         
         queue.addOperationWithBlock { () -> Void in
-            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
             
-            dispatch_async(dispatch_get_main_queue(), {
-                
+            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView4.image = img4
             })
         }
